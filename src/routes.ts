fcecd9f349571ee14600b67d5b9307b9d49bbe1a
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-27 19:40:06 -0300

    Aula: Criar autenticação do cliente

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClienteController";
 
 const router = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
 router.post("/clients", createClientController.handle);
+router.post("/clients/authenticate", authenticateClientController.handle);
 
 export { router };
