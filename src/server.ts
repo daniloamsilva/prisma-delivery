753af0967aaf830562996c9a0b084b5f7f904d85
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-27 19:49:07 -0300

    Aula: Tratando exceções

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
 import "dotenv/config";
 import { router } from "./routes";
@@ -7,5 +9,20 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
 
 app.listen(process.env.APP_PORT, () => console.log("Server is running! 🚀"));
