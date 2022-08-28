40025e006e083adb654a75c701fe3d9d34dcdd47
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-28 12:57:03 -0300

    Aula: Cadastro de deliveryman

---
+++ src/modules/deliverymen/useCases/CreateDeliverymanUseCase.ts
@@ -0,0 +1,40 @@
+import { hash } from "bcryptjs";
+
+import { prisma } from "../../../database/prismaClient";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreateDeliveryman) {
+    const deliverymanAlreadyExists = await prisma.deliverymen.findFirst({
+      where: {
+        username: {
+          contains: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (deliverymanAlreadyExists) {
+      throw new Error("Deliveryman already exists.");
+    }
+
+    const deliveryman = await prisma.deliverymen.create({
+      data: {
+        username,
+        password: await hash(password, 10),
+      },
+      select: {
+        id: true,
+        username: true,
+      },
+    });
+
+    return deliveryman;
+  }
+}
+
+export { CreateDeliverymanUseCase };
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
-import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClienteController";
+import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliverymen/useCases/CreateDeliverymanController";
 
 const router = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 router.post("/clients", createClientController.handle);
 router.post("/clients/authenticate", authenticateClientController.handle);
+router.post("/deliverymen", createDeliverymanController.handle);
 
 export { router };
